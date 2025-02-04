52a2cac60d72bab201e4c06bfd7b6f30bcde0c03
scorpion00100 <danbetheliryivuze@gmail.com>
2025-02-04 12:13:03 +0100

    Brand convention & Nominal typing

---
+++ TypeScript/task_5/js/main.ts
@@ -0,0 +1,23 @@
+interface MajorCredits {
+  credits: number;
+  __brand: 'Major';
+}
+
+interface MinorCredits {
+  credits: number;
+  __brand: 'Minor';
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: 'Major',
+  };
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: 'Minor',
+  };
+}
