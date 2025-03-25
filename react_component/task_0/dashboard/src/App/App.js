5af7eb9d6dd93fa48ffd08c2afa9fde6dd528dd4
scorpion00100 <danbetheliryivuze@gmail.com>
2025-03-25 11:20:37 +0100

    try to fix bug again

--- react_component/task_0/dashboard/src/App/App.js
+++ react_component/task_0/dashboard/src/App/App.js
@@ -1,58 +1,47 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types'
-import logo from '../assets/holberton_logo.jpg';
-import { getFullYear, getFooterCopy } from '../utils/utils';
+import React, { Component } from 'react';
 import Notifications from '../Notifications/Notifications';
-import { getLatestNotification } from '../utils/utils';
 import Login from '../Login/Login';
-import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import CourseList from '../CourseList/CourseList';
+import Header from '../Header/Header';
+import { getLatestNotification } from '../utils/utils';
 import './App.css';
+import CourseList from '../CourseList/CourseList';
 
-class App extends React.Component {
+class App extends Component {
   render() {
+    const { isLoggedIn, displayDrawer } = this.props;
+
     const listCourses = [
       { id: 1, name: 'ES6', credit: 60 },
-      { id: 2,name: 'Webpack',credit: 20},
-      {id: 3,name: 'React',credit: 40}
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
     ];
-    
+
     const listNotifications = [
-      { id: 1,type: 'default',value: 'New course available' },
-      { id: 2,type: 'urgent',value: 'New resume available' },
-      { id: 3,type: 'urgent',html: {__html: getLatestNotification()}}
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
     ];
-    const footerText = `Copyright ${getFullYear()} - ${getFooterCopy(true)}`
-    let body;
 
-    if (this.props.isLoggedIn) {
-      body = <CourseList listCourses={listCourses}/>;
-    } else {
-      body = <Login text='Login to access the full dashboard' />
-    }
     return (
-      <Fragment>
-        <Notifications listNotifications={listNotifications}/>
+      <>
         <div className="App">
-          <Header text='School dashboard' src={logo} alt='Holberton logo'/>
-          <div className="App-body">
-            {body}
+          <div className="App-head">
+            <Header />
+            <Notifications displayDrawer={displayDrawer} listNotifications={listNotifications} />
           </div>
-          <Footer text={footerText} />
-        </div>
-      </Fragment>
 
+          <div className="App-body border">
+            {isLoggedIn === false ? <Login /> : <CourseList listCourses={listCourses} />}
+          </div>
+
+          <div className="App-footer border">
+            <Footer />
+          </div>
+        </div>
+      </>
     );
   }
 }
-
-App.propTypes = {
-  isLoggedIn: PropTypes.bool
-};
-
-App.defaultProps = {
-  isLoggedIn: false
-};
 
 export default App;
--- react_component/task_0/dashboard/src/App/App.test.js
+++ react_component/task_0/dashboard/src/App/App.test.js
@@ -1,41 +1,29 @@
+// App.test.js
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from "./App";
+import App from './App';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
 
-describe('Tests the App component', () => {
-    it('Tests that App renders without crashing', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.exists()).toBe(true);
-    });
-    it('should contain the Notifications component', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find('Notifications')).toHaveLength(1);
-    });
-    it('should contain the Header component', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find('Header')).toHaveLength(1);
-    });
-    it('should contain the Login component', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find('Login')).toHaveLength(1);
-    });
-    it('should contain the Footer component', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find('Footer')).toHaveLength(1);
-    });
-    it('checks that CourseList is not displayed', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find('CourseList')).toHaveLength(0);
-    });
-});
+describe('App Component', () => {
+  it('does not display CourseList when logged out', () => {
+    const wrapper = shallow(<App isLoggedIn={false} displayDrawer={false} />);
+    expect(wrapper.find(CourseList).exists()).toBeFalsy();
+  });
 
-describe('Tests the App component when isLoggedIn is true', () => {
-    it('Tests that the Login component is not included.', () => {
-        const wrapper = shallow(<App isLoggedIn={true}/>);
-        expect(wrapper.find('Login')).toHaveLength(0);
+  describe('when isLoggedIn is true', () => {
+    let wrapper;
+
+    beforeAll(() => {
+      wrapper = shallow(<App isLoggedIn={true} displayDrawer={false} />);
     });
-    it('should contain the Notifications component', () => {
-        const wrapper = shallow(<App isLoggedIn={true}/>);
-        expect(wrapper.find('CourseList')).toHaveLength(1);
+
+    it('does not include Login component', () => {
+      expect(wrapper.find(Login).exists()).toBeFalsy();
     });
+
+    it('includes CourseList component', () => {
+      expect(wrapper.find(CourseList).exists()).toBeTruthy();
+    });
+  });
 });
