65e81a3532bfc9cae3ac18e113e7dc929f611329
scorpion00100 <danbetheliryivuze@gmail.com>
2025-03-27 09:55:08 +0100

    update config

--- React_inline_styling/task_0/dashboard/config/webpack.config.js
+++ React_inline_styling/task_0/dashboard/config/webpack.config.js
@@ -1,48 +1,43 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js'
   },
   devServer: {
+    static: {
+      directory: path.resolve(__dirname, '../dist')
+    },
     hot: true,
-    contentBase: path.resolve("./dist"),
-    compress: true,
-    port: 8564,
+    port: 8564
   },
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      },
-      {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
-          "file-loader",
+          'file-loader',
           {
-            loader: "image-webpack-loader",
+            loader: 'image-webpack-loader',
             options: {
-              bypassOnDebug: true,
-              disable: true,
-            },
-          },
-        ],
+              bypassOnDebug: true
+            }
+          }
+        ]
       },
-    ],
-  },
-};
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  }
+}
