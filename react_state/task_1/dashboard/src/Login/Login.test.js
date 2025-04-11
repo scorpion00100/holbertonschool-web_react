29974bf900c3787285d3098f2812a6d49463a2a5
scorpion00100 <danbetheliryivuze@gmail.com>
2025-04-11 09:40:39 +0200

    Controlled components and state callback

--- react_state/task_1/dashboard/src/Login/Login.test.js
+++ react_state/task_1/dashboard/src/Login/Login.test.js
@@ -7,9 +7,28 @@ describe('Tests the Login component', () => {
         const wrapper = shallow(<Login />);
         expect(wrapper.exists()).toBe(true);
     });
-    it('Tests that the component renders 2 input tags and 2 label tags', () => {
+    it('Tests that the component renders 3 input tags and 2 label tags', () => {
         const wrapper = shallow(<Login />);
         expect(wrapper.find('label')).toHaveLength(2);
-        expect(wrapper.find('input')).toHaveLength(2);
+        expect(wrapper.find('input')).toHaveLength(3);
+    });
+    it('Tests that the submit button is disabled by default', () => {
+        const wrapper = shallow(<Login />);
+        const button = wrapper.find('input[type="submit"]');
+        expect(button.props().disabled).toBe(true);
+    });
+    it('Tests that after changing the value of the two inputs, the button is enabled', () => {
+        const wrapper = shallow(<Login />);
+        const emailChangeEvent = { target: { value: 'hello@world.com' }}
+        const passwordChangeEvent = { target: { value: 'test123!' }}
+
+        const emailInput = wrapper.find('input[type="email"]');
+        emailInput.simulate('change', emailChangeEvent);
+
+        const passwordInput = wrapper.find('input[type="password"]');
+        passwordInput.simulate('change', passwordChangeEvent);
+
+        const button = wrapper.find('input[type="submit"]');
+        expect(button.props().disabled).toBe(false);
     });
 });
