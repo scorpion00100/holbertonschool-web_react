acd818809083569b046fcbcf3066748ab81e2b78
scorpion00100 <danbetheliryivuze@gmail.com>
2025-05-17 17:36:25 +0200

    refact

--- react_redux_connectors_and_providers/task_0/dashboard/src/utils/utils.js
+++
@@ -1,5 +0,0 @@
-export const getCurrentYear = () => new Date().getFullYear();
-
-export const getFooterCopy = (isIndex) => isIndex ? 'Holberton School' : 'Holberton School main dashboard';
-
-export const getLatestNotification = () => '<strong>Urgent requirement</strong> - complete by EOD'
